7ce58a5505c187897671e1983d40baaa0248574d
petridish-dg <mail@peterdg.com>
2023-05-28 18:36:48 -0500

    menu

--- app/page.js
+++ app/page.js
@@ -3,9 +3,13 @@ import Image from 'next/image';
 export default function Home() {
 	return (
 		<main>
-			<section>
+			<section className="pt-28 h-screen">
 				<div className="cntnr">
-					<h1>hello</h1>
+					<h1 className="text-5xl">
+						Portfolio of Peter de Guzman.
+						<br />
+						Digital Designer specializing in strategic design.
+					</h1>
 				</div>
 			</section>
 			<section></section>
--- components/Navbar.js
+++ components/Navbar.js
@@ -2,18 +2,17 @@ import Link from 'next/link';
 
 export default function Navbar() {
 	return (
-		<nav className="cntnr flex justify-between gap-4">
-			<div className="flex-1">
+		<nav className="cntnr grid grid-cols-8 gap-4 fixed top-0 w-full">
+			<div className="col-span-5">
 				<Link href="/" className="leading-[.1rem]">
-					Peter
-					<br />
-					De Guzman
+					<span className="font-semibold">Peter De Guzman</span>
+					<br />/ Digital Designer
 				</Link>
 			</div>
-			<div className="flex flex-col items-end flex-1">
-				<Link href="/about">About</Link>
-				<Link href="/projects">Projects</Link>
-				<Link href="/contact">Contact</Link>
+			<div className="col-span-3 flex justify-between">
+				<p>Works</p>
+				<p>About</p>
+				<p>Connect</p>
 			</div>
 		</nav>
 	);

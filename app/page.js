f80acd1dbb97812d313fbeafc9eaa9b6a490979c
Peter De Guzman <mail@peterdg.com>
2023-05-28 22:17:47 -0500

    menu update

--- app/page.js
+++ app/page.js
@@ -1,18 +1,24 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Home() {
-	return (
-		<main>
-			<section className="pt-28 h-screen">
-				<div className="cntnr">
-					<h1 className="text-5xl">
-						Portfolio of Peter de Guzman.
-						<br />
-						Digital Designer specializing in strategic design.
-					</h1>
-				</div>
-			</section>
-			<section></section>
-		</main>
-	);
+  return (
+    <main>
+      <section className="pt-28">
+        <div className="cntnr">
+          <h1 className="text-5xl">
+            Portfolio of Peter de Guzman.
+            <br />
+            Digital Designer specializing in strategic design.
+          </h1>
+        </div>
+        <div className="global-grid cntnr">
+          <div className="col-span-5">
+            <p>Chicago, IL</p>
+          </div>
+          <div className="col-span-3 text-right">2020 - 2023</div>
+        </div>
+      </section>
+      <section className="h-screen"></section>
+    </main>
+  );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,24 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Navbar() {
-	return (
-		<nav className="cntnr grid grid-cols-8 gap-4 fixed top-0 w-full">
-			<div className="col-span-5">
-				<Link href="/" className="leading-[.1rem]">
-					<span className="font-semibold">Peter De Guzman</span>
-					<br />/ Digital Designer
-				</Link>
-			</div>
-			<div className="col-span-3 flex justify-between">
-				<p>Works</p>
-				<p>About</p>
-				<p>Connect</p>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="w-full ">
+      <div className="flex h-8 w-full items-center justify-center bg-lime-400 text-sm uppercase text-black">
+        <p>Open and Available for Works + Collaborations</p>
+      </div>
+      <div className="cntnr global-grid gap-4 py-6">
+        <div className="lg:col-span-5">
+          <Link href="/" className="leading-[.1rem]">
+            <span className="font-semibold">Peter De Guzman</span>
+            <br />/ Digital Designer
+          </Link>
+        </div>
+        <div className="flex justify-between lg:col-span-3">
+          <p>Works</p>
+          <p>About</p>
+          <p>Connect</p>
+        </div>
+      </div>
+    </nav>
+  );
 }
